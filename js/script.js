7816a6c3f2be9ce18cfe214ffea91f07d5d6357b
Brayan Bernal <BBernal@OSSF.LOCAL>
2018-03-28 09:41:33 -0400

    Haciendo Cambios

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
+const $body = document.body;
 const $burguerButton = document.getElementById('burguer-button')
 const $menu = document.getElementById('menu')
 let consulta = window.matchMedia('(max-width: 500px)')
 let toggleMenu=()=>{$menu.classList.toggle('active')}
-
+let showMenu=()=>{$menu.classList.add('active')}
+let hideMenu =()=>{$menu.classList.remove('active')}
 function mediaQuerie() {
     if (consulta.matches) {
-        console.log('a')
         $burguerButton.addEventListener('touchstart',toggleMenu)
     } else {
-        console.log('b')
         $burguerButton.removeEventListener('touchstart',toggleMenu)
     } 
 }
+
 consulta.addListener(mediaQuerie)
 mediaQuerie()
 
 var bLazy = new Blazy({
     selector: 'img'
 });
+let gestos = new Hammer($body);
+gestos.on('swipeleft',hideMenu)
+gestos.on('swiperight',showMenu)
